af47ade4b7b8a0f004a4712916fb2f57fed5db45
fortuna <fortuna@localhost>
2010-08-19 21:17:59 +1000

    Refactored test scripts

--- src/test/groovy/org/mnode/ousia/SyntaxAreaTest.groovy
+++ src/test/groovy/org/mnode/ousia/SyntaxAreaTest.groovy
@@ -2,25 +2,20 @@ package org.mnode.ousia
 
 import javax.swing.JFrame;
 
-class SyntaxAreaTest {
+def ousia = new OusiaBuilder()
 
-    public static void main(String[] args) {
-        def ousia = new OusiaBuilder()
-        
-        ousia.edt {
-            lookAndFeel('substance-nebula')
-            
-            frame(title: 'SyntaxAreaTest', pack: true, show: true, defaultCloseOperation: JFrame.EXIT_ON_CLOSE) {
-                rSyntaxScrollPane(id: 'sp') {
-                    sp.gutter.bookmarkingEnabled = true
-                    sp.gutter.bookmarkIcon = imageIcon('/bookmark.png')
-                    
-                    rSyntaxTextArea('This is a test', marginLineEnabled: true, id: 'ta') {
-                        ta.addHyperlinkListener(new HyperlinkBrowser())
-                        ta.syntaxEditingStyle = 'text/groovy'
-                    }
-                }
-            }
-        }
-    }
+ousia.edt {
+    lookAndFeel('substance-nebula')
+
+	frame(title: 'SyntaxAreaTest', pack: true, show: true, defaultCloseOperation: JFrame.EXIT_ON_CLOSE) {
+	    rSyntaxScrollPane(id: 'sp') {
+	        sp.gutter.bookmarkingEnabled = true
+	        sp.gutter.bookmarkIcon = imageIcon('/bookmark.png')
+	        
+	        rSyntaxTextArea('This is a test', marginLineEnabled: true, id: 'ta') {
+	            ta.addHyperlinkListener(new HyperlinkBrowser())
+	            ta.syntaxEditingStyle = 'text/groovy'
+	        }
+	    }
+	}
 }
